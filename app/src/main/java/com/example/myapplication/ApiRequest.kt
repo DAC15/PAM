e31d99b9cebb82eb28ac6f48b3fd6854f1cc3893
DAC15 <rusudacian95@gmail.com>
2021-04-23 09:12:47 +0300

    Implemented a simple API using  Retrofit(https://square.github.io/retrofit/)

---
+++ app/src/main/java/com/example/myapplication/ApiRequest.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import com.example.myapplication.api.CatJson
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiRequest {
+
+    @GET("/facts/random")
+    fun getCatFacts() : Call<CatJson>
+}
